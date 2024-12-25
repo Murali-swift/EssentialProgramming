d40ce3061d34692500f56007b9c9612885bb943f
Murali Nallusamy <mnallusamy@opentext.com>
2024-12-25 22:18:18 +0530

    move remotefeedloader to production code

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,25 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Murali Nallusamy on 25/12/24.
+//
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url:URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from:url)
+    }
+}
