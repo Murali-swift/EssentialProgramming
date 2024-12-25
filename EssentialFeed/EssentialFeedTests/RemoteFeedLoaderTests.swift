2f734ff84e327f32588f9645d9dfbf0e0acf42ab
Murali Nallusamy <mnallusamy@opentext.com>
2024-12-25 22:09:26 +0530

    Refactored the Remotefeedloader

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,10 +10,12 @@ import XCTest
 class RemoteFeedLoader {
     let client: HTTPClient
     let url: URL
-    init(url: URL = URL(string: "https://www.google.com")!, client: HTTPClient) {
+    
+    init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
+    
     func load() {
         client.get(from:url)
     }
@@ -22,8 +24,6 @@ class RemoteFeedLoader {
 protocol HTTPClient {
     func get(from url:URL)
 }
-
-
 
 
 class RemoteFeedLoaderTests: XCTestCase {
